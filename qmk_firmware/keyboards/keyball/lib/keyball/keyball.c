a05b1e3a55e89eed635ed8c516d6a5a5a663754f
rm86choppat <166978804+rm86choppat@users.noreply.github.com>
2025-02-19 20:11:28 +0900

    reverse_scrolling_direction win,linuxのみmacのトラックパッドに合わせてスクロールの向きを反転(無条件にするとmacで、逆になってしまうため)

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -233,10 +233,12 @@ __attribute__((weak)) void keyball_on_apply_motion_to_mouse_scroll(keyball_motio
             break;
     }
 #endif
-    // スクロール反転
-    // https://zenn.dev/yamavel/articles/79dbe9a82c98ea
-    r->v = -r->v;
-    r->h = -r->h;
+    // windowsOSでスクロール方向反転
+    // https://qiita.com/toxaO/items/a46d04a476d17975dee1
+    if (detected_host_os() == OS_WINDOWS || detected_host_os() == OS_LINUX){
+        r->h = -r->h;
+        r->v = -r->v;
+    }
 }
 
 static void motion_to_mouse(keyball_motion_t *m, report_mouse_t *r, bool is_left, bool as_scroll) {

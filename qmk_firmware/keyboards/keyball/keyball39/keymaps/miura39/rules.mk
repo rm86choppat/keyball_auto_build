6edde95692de5e85975b648ae3bf72327d7f31a1
rm86choppat <166978804+rm86choppat@users.noreply.github.com>
2025-04-29 17:40:06 +0900

    keep_aml_until_keypress トラックボール動かなくなったのでデバッグ指定一旦コメント

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/miura39/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/miura39/rules.mk
@@ -6,7 +6,7 @@ VIA_ENABLE = yes
 
 OS_DETECTION_ENABLE = yes
 
-CONSOLE_ENABLE = yes
+# CONSOLE_ENABLE = yes
 # 以下入れた後にトラックボール動かなくなったと入れても容量削減もできなかったから一旦コメント
 # LTO_ENABLE = yes
 # EXTRAFLAGS += -flto
